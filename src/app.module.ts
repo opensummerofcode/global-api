675b4632ef10d7c3bf97c76dc958dad9de352862
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-07-11 09:38:21 +0200

    Set up GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+// import { UsersService } from './users/users.resolver';
+// import { ChaptersService } from './chapters/chapters.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+  ],
+  // providers: [UsersService, ChaptersService],
 })
 export class AppModule {}
