1776cf766e75b8d9d99c79179ab9cba270116f6e
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-07-11 09:49:53 +0200

    Set up mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     UserModule,
     GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+    MongooseModule.forRoot('{put-mongodb-uri-here}'),
   ],
   // providers: [UsersService, ChaptersService],
 })
