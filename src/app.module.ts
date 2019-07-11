3291e0265ee710a17d0648128c2f466b63854d62
Ismaila Abdoulahi <ismaila.abdoulahi@student.vinci.be>
2019-07-11 11:08:34 +0200

    Set up dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { config } from 'dotenv';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+
+config();
 
 @Module({
   imports: [
     UserModule,
     GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
-    MongooseModule.forRoot('{put-mongodb-uri-here}'),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      dbName: process.env.DB_NAME,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    }),
   ],
   // providers: [UsersService, ChaptersService],
 })
